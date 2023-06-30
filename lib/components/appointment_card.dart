62baad8068670ad049f8f56a16d45f5a7f23c553
raunak-lab <raunakkumarthakur12@gmail.com>
2023-06-30 18:00:13 +0530

    improved appointment card

--- lib/components/appointment_card.dart
+++ lib/components/appointment_card.dart
@@ -105,21 +105,21 @@ class ScheduleCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: const [
           Icon(Icons.calendar_today
-          ,color:Colors.white,size: 15),
+              ,color:Colors.white,size: 15),
           SizedBox(width: 5),
           Text(
             'Monday, 11/28/2022',
             style:  TextStyle(
-              color: Colors.white
+                color: Colors.white
             ),
           ),
-           SizedBox(width: 20,),
-           Icon(Icons.access_alarm,
-          color: Colors.white,
-          size: 17,),
+          SizedBox(width: 20,),
+          Icon(Icons.access_alarm,
+            color: Colors.white,
+            size: 17,),
           SizedBox(width: 5,),
           Flexible(child: Text('2:00 PM',style: TextStyle(color: Colors.white),))
-          
+
 
 
         ],
